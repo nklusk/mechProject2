f19ce048a2a71d6dd6ac95eaa24fb7f23b9f49b9
NATALIE LUSK <NKL22@fsu.edu>
2024-02-06 14:11:23 -0500

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+var host = '127.0.0.1:8888';
+var personOrNot
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  
+
+
+  // connect to server...
+  socket = new WebSocket('ws://' + host);
+  socket.onopen = openHandler;
+  socket.onmessage = messageHandler;
+  
 }
 
-function draw() {
-  background(125);
+function openHandler() {
+  console.log("Connected to socket server at " + host);
 }
+
+function messageHandler(event) {
+  var msg = event.data; // read data from the onmessage event
+  personOrNot = msg // converts received message to a note value
+  console.log(personOrNot);
+  
+}
+
+
+function draw() {
+  background(255);
+  //image(capture, 0, 0, 320, 240);
+  //filter(INVERT);
+
+  if (personOrNot == 1){
+    image(capture, 0, 0, 320, 240);
+  } else {
+    capture.hide();
+  }
+}
+
